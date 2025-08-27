b35f5f35ec027ab7618d11b4aaca9f4b8581d4fb
Alexander <moniki2139@yandex.ru>
2025-08-27 18:38:33 +0300

    Solved 2025-08-19

---
+++ LC_Daily/Problems/2025/2025-08/2025-08-19/Problem_Zero_Subarrays.cs
@@ -0,0 +1,75 @@
+﻿using Problems;
+
+namespace LC_Daily.Problems._2025._2025_08._2025_08_19
+{
+    [Problem("2025-08-19")]
+    public class Problem_Zero_Subarrays : IProblem
+    {
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            int State = 0;
+            long result = 0;
+            nums = nums.Append(1).ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                switch (nums[i])
+                {
+                    case 0:
+                        State++;
+                        break;
+                    default:
+                        result += ArithmeticSum(State);
+                        State = 0;
+                        break;
+                }
+            }
+            return result;
+        }
+
+        public static long ArithmeticSum(long n)
+        {
+            return n < 1 ? 0 : n * (n + 1) / 2;
+        }
+
+        public void Execute()
+        {
+            List<int> nums = [];
+            while (true)
+            {
+                Console.WriteLine("---------------- 2348 ----------------");
+                Console.WriteLine("Введите числа через запятую:");
+
+                if (Console.ReadLine() is not string input) continue;
+
+                if (input == "exit")
+                {
+                    Console.WriteLine("---------------- end ----------------\n");
+                    return;
+                }
+
+                bool failed = false;
+                foreach (var item in input.Split(","))
+                {
+                    if (!int.TryParse(item, out int num))
+                    {
+                        failed = true;
+                        break;
+                    }
+
+                    nums.Add(num);
+                }
+
+                if (failed)
+                {
+                    Console.WriteLine("Ошибка ввода, повторите попытку или выйдите из меню через exit");
+                    Console.WriteLine("---------------- end ----------------\n");
+                    continue;
+                }
+
+                Console.WriteLine($"Result: {ZeroFilledSubarray([.. nums])}");
+                Console.WriteLine("---------------- end ----------------\n");
+            }
+        }
+    }
+}
---
+++ LC_Daily_Tests/2025/2025-08/2025-08-19/Problem_Zero_Subarrays_Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LC_Daily.Problems._2025._2025_08._2025_08_19;
+using Problems;
+
+namespace LC_Daily_Tests._2025._2025_08._2025_08_19
+{
+    public class Problem_Zero_Subarrays_Tests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var solver = new Problem_Zero_Subarrays();
+
+            var result = solver.ZeroFilledSubarray([1, 3, 0, 0, 2, 0, 0, 4]);
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Example2()
+        {
+            var solver = new Problem_Zero_Subarrays();
+
+            var result = solver.ZeroFilledSubarray([0, 0, 0, 2, 0, 0]);
+
+            Assert.Equal(9, result);
+        }
+
+        [Fact]
+        public void Example3()
+        {
+            var solver = new Problem_Zero_Subarrays();
+
+            var result = solver.ZeroFilledSubarray([2, 10, 2019]);
+
+            Assert.Equal(0, result);
+        }
+    }
+}
